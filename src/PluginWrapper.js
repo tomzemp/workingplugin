877fbb0bf432392c443d53e739087c9377aef477
Thomas Zemp <tom@dhis2.org>
2023-01-31 17:09:26 +0100

    fix: remove unused files

--- src/PluginWrapper.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export const PluginWrapper = ({requiredProps, children}) => {
-	const propsFromParent = {setWidth:(msg)=>{console.log('cat')}}
-	return children({...propsFromParent})
-}
